ae2aa4505cd6eeea4313c30c63103dfd7060b643
AlexeyKrasnopevtsev <fktrctq321@gmail.com>
2026-03-19 18:13:35 +0700

    Add file parsing

---
+++ src/Parser.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Hexlet\Code;
+
+function parseFile(string $filepath): object
+{
+    if (!file_exists($filepath)) {
+        throw new \Exception("File not found: {$filepath}");
+    }
+    
+    $content = file_get_contents($filepath);
+    
+    if ($content === false) {
+        throw new \Exception("Cannot read file: {$filepath}");
+    }
+    
+    $data = json_decode($content);
+    
+    if ($data === null) {
+        throw new \Exception("Invalid JSON in file: {$filepath}");
+    }
+    
+    return $data;
+}
